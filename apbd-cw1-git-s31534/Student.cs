da7dbaae579d15f13613fb77a9ca7db8888dd411
melihio <melihcokan@icloud.com>
2026-03-10 10:58:29 +0100

    feat: GetAverage

--- apbd-cw1-git-s31534/Student.cs
+++ apbd-cw1-git-s31534/Student.cs
@@ -3,5 +3,21 @@ namespace apbd_cw1_git_s31534;
 public class Student
 {
     public String Name { get; set; }
-    public int grade { get; set; }
+    public int Grade { get; set; }
+
+    public Student(String name, int grade)
+    {
+        Name = name;
+        Grade = grade;
+    }
+
+    static int GetAverage(List<Student> students)
+    {
+        int sum = 0;
+        foreach (var student in students)
+        {
+            sum += student.Grade;
+        }
+        return sum / students.Count;
+    }
 }
