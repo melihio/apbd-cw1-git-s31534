6a36245a19f48017b4b1ea18e8fb15fbb6e727b2
melihio <melihcokan@icloud.com>
2026-03-10 11:01:32 +0100

    Class added

--- apbd-cw1-git-s31534/Class.cs
+++ apbd-cw1-git-s31534/Class.cs
@@ -2,5 +2,12 @@ namespace apbd_cw1_git_s31534;
 
 public class Class
 {
-    
+    public Teacher Teacher;
+    public List<Student> Students;
+
+    public Class(Teacher teacher, List<Student> students)
+    {
+        Teacher = teacher;
+        Students = students;
+    }
 }
